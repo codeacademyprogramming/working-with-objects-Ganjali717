668a71cac9d00da7d7bd61f832236f134e89f28a
Ganjali Imanov <34399229+Ganjali717@users.noreply.github.com>
2021-08-14 18:21:30 +0400

    Add files via upload
    HomeWork

---
+++ script.js
@@ -0,0 +1,52 @@
+
+// 1.Task 
+// let user = {
+//     name: "John",
+//     surname: "Smith"
+// }
+
+// user.name = "Pete";
+
+// let key = "name";
+// delete user["name"];
+// console.log(user);
+
+// 2.Task
+// function isEmpty(user) {
+//     for(let key in user){
+//         return false; 
+//     }
+//     return true;
+// }
+
+// 3. Task
+// let salaries = {
+//     John: 100,
+//     Ann: 160,
+//     Pete: 130
+//   }
+
+//    let sum = 0;
+//    for(let key in salaries){
+//      sum += salaries[key];
+//    }
+  
+// console.log(sum);
+
+// 4. Task 
+// let menu = {
+//     width: 200,
+//     height: 300,
+//     title: "My menu"
+//   };
+
+// function multiplyNumeric(menu){
+//     for(let key in menu){
+//     if (typeof menu[key] == 'number') {
+//            menu[key] *= 2;
+//           }
+//     }
+//     return menu;
+// };
+
+// console.log(multiplyNumeric(menu));
